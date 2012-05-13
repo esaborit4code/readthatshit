c1c650cdcac6f6e127abc88405394d23b372f600
saborit.eduardo@gmail.com <saborit.eduardo@gmail.com@b9ac0532-b296-deb0-b7b3-8a33a806bde0>
2012-05-13 16:50:02 +0000

    Now is possible to handle multiple RSS lists with custom names

--- trunk/rss.php
+++ trunk/rss.php
@@ -1,45 +1,49 @@
 <?php
+$database_folder_path = "database";
+
 function get_items_xml($items) {
 	$xml = "";
 	foreach($items as $item)
 	{
-		$xml .= 
-			'<item>
-				<title><![CDATA[' . $item->title . ']]></title>
-				<link><![CDATA[' . $item->url . ']]></link>
-			</item>';
+		$xml .= '
+		<item>
+		<title><![CDATA[' . $item->title . ']]></title>
+		<link><![CDATA[' . $item->url . ']]></link>
+		</item>';
 	}
 
 	return $xml;
 }
-function get_items_from_file($file_name)
-{
-	$file_content = file_get_contents($file_name);
 
-	$json = "[" . str_replace("\n{", ",{", $file_content) . "]";
+function get_list($machine_friendly_name) {
+	global $database_folder_path;
 
-	$items = json_decode($json);
+	$list_file_name = $database_folder_path . "/" . $machine_friendly_name;
 
-	return $items;
+	$json_string = file_get_contents($list_file_name);
+
+	$list = json_decode($json_string);
+
+	return $list;
 }
-function get_rss_xml_from_file($file_name)
-{
-	$items = get_items_from_file($file_name);
 
-	$items_xml = get_items_xml($items);	
-	$xml = 
-		'<?xml version="1.0" encoding="utf-8"?>
-		<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">
-			<channel>
-				<title><![CDATA[Read That Shit]]></title>
-				<link><![CDATA[readthatshit.com]]></link> 
-				<description><![CDATA[Read That Shit]]></description>'
-				. $items_xml .
-			"</channel>
-		</rss>";
-	
+function get_rss_for_list($machine_friendly_name) {
+	$list = get_list($machine_friendly_name);
+
+	$items_xml = get_items_xml($list->items);
+	$xml =
+	'<?xml version="1.0" encoding="utf-8"?>
+	<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">
+	<channel>
+	<title><![CDATA[' . $list->title . ']]></title>
+	<link><![CDATA[readthatshit.com/list.html#' . $machine_friendly_name . ']]></link>
+	<description><![CDATA[' . $list->description . ']]></description>'
+	. $items_xml .
+	"</channel>
+	</rss>";
+
 	return $xml;
 }
 
-$rss_xml = get_rss_xml_from_file("database");
+$rss_xml = get_rss_for_list($_GET["list"]);
 echo $rss_xml;
--- trunk/save.php
+++ trunk/save.php
@@ -1,16 +1,45 @@
 <?php
-function save_entry($title, $url)
-{
-	$database_file_name = "database";
-	$database_file_resource = fopen($database_file_name, 'a');
+$database_folder_path = "database";
 
-	$string_data = json_encode(array("title" => $title, "url" => $url));
+function get_list($machine_friendly_name) {
+	global $database_folder_path;
 
-	fwrite($database_file_resource, $string_data . "\n");	
-	fclose($database_file_resource);
+	$list_file_name = $database_folder_path . "/" . $machine_friendly_name;
 
-	return $string_data;
+	$json_string = file_get_contents($list_file_name);
+
+	$list = json_decode($json_string);
+
+	return $list;
 }
 
-$string_data = save_entry($_POST["title"], $_POST["url"]);
+function save_list($machine_friendly_name, $list) {
+	global $database_folder_path;
+
+	$json_string = json_encode($list);
+
+	$list_file_name = $database_folder_path . "/" . $machine_friendly_name;
+
+	$file_handle = fopen($list_file_name, 'w');
+
+	fwrite($file_handle, $json_string);
+	fclose($file_handle);
+}
+
+function add_entry($machine_friendly_name, $title, $url)
+{
+	$list = get_list($machine_friendly_name);
+
+	$item = array("title" => $title, "url" => $url);
+
+	$list->items[] = $item;
+
+	save_list($machine_friendly_name, $list);
+
+	$item_json_string = json_encode($item);
+
+	return $item_json_string;
+}
+
+$string_data = add_entry($_POST["machine_friendly_name"], $_POST["title"], $_POST["url"]);
 echo $string_data;
