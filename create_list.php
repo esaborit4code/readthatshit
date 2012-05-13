109a02770b4ea72fe11fb04ea8029130b438351b
Edu Saborit <esaborit4code@users.noreply.github.com>
2012-05-13 16:50:02 +0000

    Now is possible to handle multiple RSS lists with custom names

---
+++ create_list.php
@@ -0,0 +1,82 @@
+<?php
+$database_folder_path = "database";
+
+/* TODO: This should go to a helper class */
+function get_machine_friendly_name($title) {
+	$machine_friendly_name = $title;
+
+	$spacing_characters = array(' ', '&', '\r\n', '\n', '+',',');
+
+	$special_characters = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'ß', 'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'Ā', 'ā', 'Ă', 'ă', 'Ą', 'ą', 'Ć', 'ć', 'Ĉ', 'ĉ', 'Ċ', 'ċ', 'Č', 'č', 'Ď', 'ď', 'Đ', 'đ', 'Ē', 'ē', 'Ĕ', 'ĕ', 'Ė', 'ė', 'Ę', 'ę', 'Ě', 'ě', 'Ĝ', 'ĝ', 'Ğ', 'ğ', 'Ġ', 'ġ', 'Ģ', 'ģ', 'Ĥ', 'ĥ', 'Ħ', 'ħ', 'Ĩ', 'ĩ', 'Ī', 'ī', 'Ĭ', 'ĭ', 'Į', 'į', 'İ', 'ı', 'Ĳ', 'ĳ', 'Ĵ', 'ĵ', 'Ķ', 'ķ', 'Ĺ', 'ĺ', 'Ļ', 'ļ', 'Ľ', 'ľ', 'Ŀ', 'ŀ', 'Ł', 'ł', 'Ń', 'ń', 'Ņ', 'ņ', 'Ň', 'ň', 'ŉ', 'Ō', 'ō', 'Ŏ', 'ŏ', 'Ő', 'ő', 'Œ', 'œ', 'Ŕ', 'ŕ', 'Ŗ', 'ŗ', 'Ř', 'ř', 'Ś', 'ś', 'Ŝ', 'ŝ', 'Ş', 'ş', 'Š', 'š', 'Ţ', 'ţ', 'Ť', 'ť', 'Ŧ', 'ŧ', 'Ũ', 'ũ', 'Ū', 'ū', 'Ŭ', 'ŭ', 'Ů', 'ů', 'Ű', 'ű', 'Ų', 'ų', 'Ŵ', 'ŵ', 'Ŷ', 'ŷ', 'Ÿ', 'Ź', 'ź', 'Ż', 'ż', 'Ž', 'ž', 'ſ', 'ƒ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ǎ', 'ǎ', 'Ǐ', 'ǐ', 'Ǒ', 'ǒ', 'Ǔ', 'ǔ', 'Ǖ', 'ǖ', 'Ǘ', 'ǘ', 'Ǚ', 'ǚ', 'Ǜ', 'ǜ', 'Ǻ', 'ǻ', 'Ǽ', 'ǽ', 'Ǿ', 'ǿ');
+	$common_characters = array('A', 'A', 'A', 'A', 'A', 'A', 'AE', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'D', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 's', 'a', 'a', 'a', 'a', 'a', 'a', 'ae', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'n', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', 'A', 'a', 'A', 'a', 'A', 'a', 'C', 'c', 'C', 'c', 'C', 'c', 'C', 'c', 'D', 'd', 'D', 'd', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'G', 'g', 'G', 'g', 'G', 'g', 'G', 'g', 'H', 'h', 'H', 'h', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'IJ', 'ij', 'J', 'j', 'K', 'k', 'L', 'l', 'L', 'l', 'L', 'l', 'L', 'l', 'l', 'l', 'N', 'n', 'N', 'n', 'N', 'n', 'n', 'O', 'o', 'O', 'o', 'O', 'o', 'OE', 'oe', 'R', 'r', 'R', 'r', 'R', 'r', 'S', 's', 'S', 's', 'S', 's', 'S', 's', 'T', 't', 'T', 't', 'T', 't', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'W', 'w', 'Y', 'y', 'Y', 'Z', 'z', 'Z', 'z', 'Z', 'z', 's', 'f', 'O', 'o', 'U', 'u', 'A', 'a', 'I', 'i', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'A', 'a', 'AE', 'ae', 'O', 'o');
+
+	$some_other_regex_to_replace = array('/[^a-z0-9\-<>]/', '/[\-]+/', '/<[^>]*>/');
+	$regex_replacement = array('', '-', '');
+
+	$machine_friendly_name = strtolower(trim($machine_friendly_name));
+	$machine_friendly_name = html_entity_decode($machine_friendly_name, ENT_QUOTES, 'UTF8');
+	$machine_friendly_name = str_replace($special_characters, $common_characters, $machine_friendly_name);
+	$machine_friendly_name = str_replace ($spacing_characters, '-', $machine_friendly_name);
+	$machine_friendly_name = preg_replace ($some_other_regex_to_replace, $regex_replacement, $machine_friendly_name);
+
+	return $machine_friendly_name;
+}
+
+function check_if_list_exists($machine_friendly_name) {
+	global $database_folder_path;
+
+	$list_file_name = $database_folder_path . "/" . $machine_friendly_name;
+
+	return file_exists($list_file_name);
+}
+
+function create_list_file($machine_friendly_name) {
+	global $database_folder_path;
+
+	$list_file_name = $database_folder_path . "/" . $machine_friendly_name;
+
+	$file_handle = fopen($list_file_name, 'w');
+	fclose($file_handle);
+
+	return file_exists($list_file_name);
+}
+
+function save_list_data($machine_friendly_name, $title, $description)
+{
+	global $database_folder_path;
+
+	$json_string = json_encode(array("title" => $title, "description" => $description, "items" => array()));
+
+	$list_file_name = $database_folder_path . "/" . $machine_friendly_name;
+
+	$file_handle = fopen($list_file_name, 'w');
+
+	fwrite($file_handle, $json_string);
+	fclose($file_handle);
+}
+
+function create_list($title, $description)
+{
+	$machine_friendly_name = get_machine_friendly_name($title);
+
+	$name_already_used = check_if_list_exists($machine_friendly_name);
+
+	if($name_already_used) {
+		$response_array = array("errors" => array("LIST NAME ALREADY USED"));
+		$json_string = json_encode($response_array);
+
+		return $json_string;
+	}
+
+	create_list_file($machine_friendly_name);
+
+	save_list_data($machine_friendly_name, $title, $description);
+
+	$response_array = array("title" => $title, "description" => $description, "machine_friendly_name" => $machine_friendly_name);
+	$json_string = json_encode($response_array);
+
+	return $json_string;
+}
+
+$string_data = create_list($_POST["title"], $_POST["description"]);
+echo $string_data;
