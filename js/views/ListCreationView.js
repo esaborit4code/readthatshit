784e909ec068e5b7caa5053d2fc406b7c2755f11
Edu Saborit <esaborit4code@users.noreply.github.com>
2012-05-26 18:18:28 +0000

    Added Config.js to save configuration parameters

--- js/views/ListCreationView.js
+++ js/views/ListCreationView.js
@@ -1,2 +1,78 @@
-var ListCreationView = function() {};
-ListCreationView.prototype.template = "";
+var ListCreationView = function() {
+    this.initialize();
+};
+
+ListCreationView.TEMPLATE_LOAD_ERROR = "Could not load tempate file.";
+ListCreationView.INVALID_TEMPLATE = "Invalid template.";
+
+ListCreationView.template_file = "ListCreationView.html";
+ListCreationView.template_html = null;
+
+ListCreationView.prototype.initialize = function() {
+    this.element = ListCreationView.getElementFromTemplateHtml();
+    this.initializeForm();
+    this.initializeSaveButton();
+};
+
+ListCreationView.getElementFromTemplateHtml = function() {
+    var static_template_not_loaded = (ListCreationView.template_html == null);
+    if(static_template_not_loaded) {
+        ListCreationView.loadTemplateHtml();
+    }
+    
+    var auxiliar = document.createElement("div");
+    auxiliar.innerHTML = ListCreationView.template_html;
+    
+    var element = auxiliar.firstChild;
+    
+    return element;
+};
+
+ListCreationView.loadTemplateHtml = function() {
+    $.ajax({
+        url: Config.HtmlTemplatesPath + "/" + ListCreationView.template_file,
+        async: false,
+        success: ListCreationView.onTemplateFileLoaded,
+        error: ListCreationView.onTemplateFileError
+    });
+};
+
+ListCreationView.onTemplateFileLoaded = function(file_content) {
+    var auxiliar = document.createElement("div");
+    auxiliar.innerHTML = file_content;
+    
+    var template_is_only_text = (auxiliar.children[0] == null);
+    if(template_is_only_text) {
+        throw new Error(ListCreationView.INVALID_TEMPLATE);
+    }
+    
+    ListCreationView.template_html = file_content;
+};
+
+ListCreationView.onTemplateFileError = function(file_content) {
+    throw new Error(ListCreationView.TEMPLATE_LOAD_ERROR);
+};
+
+ListCreationView.prototype.initializeForm = function() {
+    this.form = $(this.element).find("form").get(0);
+};
+
+ListCreationView.prototype.initializeSaveButton = function() {
+    this.save_button = $(this.element).find(".save.button").get(0);
+    this.save_button.addEventListener("click", this.onSaveButtonClick.bind(this));
+};
+
+ListCreationView.prototype.onSaveButtonClick = function(click_event) {
+    var event = new Events.ButtonClick(this.save_button);
+    
+    Bus.broadcast(event);
+};
+
+ListCreationView.prototype.getData = function() {
+    var data = {
+        "title": this.form.title.value,
+        "description": this.form.description.value
+    };
+    
+    return data;
+};
--- test/js/spec/ListCreationViewSpec.js
+++ test/js/spec/ListCreationViewSpec.js
@@ -1,6 +1,116 @@
 describe("ListCreationView", function() {
-    it("should have a template", function() {
-        var a_view = new ListCreationView();
-        expect(a_view.template).toNotEqual(null);
+    var the_view;
+    var original_bus = copyClass(Bus);
+    var original_$_ajax = $.ajax;
+    
+    beforeEach(function(){
+        Bus = original_bus;
+        $.ajax = original_$_ajax;
+
+        the_view = new ListCreationView();
+    });
+    
+    it("should get its template via ajax when instance", function() {
+        ListCreationView.template_html = null;
+        
+        ajax_params_passed = null;
+        $.ajax = function(params) {
+            ajax_params_passed = params;
+            original_$_ajax(params);
+        };
+        
+        new ListCreationView();
+        
+        var template_full_path = Config.HtmlTemplatesPath + "/" + ListCreationView.template_file;
+        expect(ajax_params_passed.url).toEqual(template_full_path);
+    });
+    
+    it("should get its template via ajax when first instance", function() {
+        ListCreationView.template_html = null;
+        
+        called_times = 0;
+        $.ajax = function(params) {
+            called_times++;
+            original_$_ajax(params);
+        };
+        
+        new ListCreationView();
+        new ListCreationView();
+        
+        expect(called_times).toEqual(1);
+    });
+    
+    it("should throw an exception when loading an invalid template", function() {
+        ListCreationView.template_html = null;
+        
+        $.ajax = function(params) {
+            params.success("dummy invalid template");
+        };
+        
+        expect(function() {
+            new ListCreationView();
+        }).toThrow(ListCreationView.INVALID_TEMPLATE);
+    });
+      
+    it("should throw an exception when getting template via ajax call fails", function() {
+        ListCreationView.template_html = null;
+        
+        $.ajax = function(params) {
+            params.error();
+        };
+        
+        expect(function() {
+            new ListCreationView();
+        }).toThrow(ListCreationView.TEMPLATE_LOAD_ERROR);
+    });
+    
+    it("should have an element", function() {
+        expect(the_view.element).toNotEqual(null);
+    });
+    
+    it("should have a valid html element as element", function() {
+        var thrown_exception = null;
+        try {
+            Html.checkIfValidHTMLElement(the_view.element);
+        }
+        catch(exception) {
+            thrown_exception = exception;
+        }
+        expect(thrown_exception).toBeNull();
+    });
+    
+    it("should have a form", function() {
+        expect(the_view.form).toNotEqual(null);
+        expect(the_view.form.tagName.toLowerCase()).toEqual("form");
+    });
+
+    it("should have a save button", function() {
+        expect(the_view.save_button).toNotEqual(null);
+    });
+
+    it("should send an event when clicking on save button", function() {
+        var broadcasted_event = null;
+        Bus.broadcast = function(event) {
+            broadcasted_event = event;  
+        };
+        
+        the_view.save_button.click();
+        
+        expect(broadcasted_event instanceof Events.ButtonClick).toBeTruthy();
+        expect(broadcasted_event.button).toEqual(the_view.save_button);
+    });
+    
+    it("should return its data", function() {
+        var any_title = "any title";
+        var any_description = "any description";
+        
+        the_view.form.title.value = any_title;
+        the_view.form.description.value = any_description;
+        
+        var data = the_view.getData();
+        
+        var expected_data = { "title": any_title, "description": any_description };
+        
+        expect(data).toEqual(expected_data);
     });
 });
