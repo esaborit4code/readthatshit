f9de33786ede4d5cc2666d6673f23f72cb9da458
saborit.eduardo@gmail.com <saborit.eduardo@gmail.com@b9ac0532-b296-deb0-b7b3-8a33a806bde0>
2012-05-27 15:52:09 +0000

    Created ViewLoader while refactoring EntryAdditionView and ListCreationView in the name of DRY principle

--- trunk/js/views/EntryAdditionView.js
+++ trunk/js/views/EntryAdditionView.js
@@ -2,55 +2,12 @@ var EntryAdditionView = function() {
     this.initialize();
 };
 
-EntryAdditionView.TEMPLATE_LOAD_ERROR = "Could not load tempate file.";
-EntryAdditionView.INVALID_TEMPLATE = "Invalid template.";
-
 EntryAdditionView.template_file = "EntryAdditionView.html";
-EntryAdditionView.template_html = null;
 
 EntryAdditionView.prototype.initialize = function() {
-    this.element = EntryAdditionView.getElementFromTemplateHtml();
+    this.element = ViewLoader.getElementFromTemplateFile(EntryAdditionView.template_file);
     this.initializeForm();
     this.initializeSaveButton();
-};
-
-EntryAdditionView.getElementFromTemplateHtml = function() {
-    var static_template_not_loaded = (EntryAdditionView.template_html == null);
-    if(static_template_not_loaded) {
-        EntryAdditionView.loadTemplateHtml();
-    }
-    
-    var auxiliar = document.createElement("div");
-    auxiliar.innerHTML = EntryAdditionView.template_html;
-    
-    var element = auxiliar.firstChild;
-    
-    return element;
-};
-
-EntryAdditionView.loadTemplateHtml = function() {
-    $.ajax({
-        url: Config.HtmlTemplatesPath + "/" + EntryAdditionView.template_file,
-        async: false,
-        success: EntryAdditionView.onTemplateFileLoaded,
-        error: EntryAdditionView.onTemplateFileError
-    });
-};
-
-EntryAdditionView.onTemplateFileLoaded = function(file_content) {
-    var auxiliar = document.createElement("div");
-    auxiliar.innerHTML = file_content;
-    
-    var template_is_only_text = (auxiliar.children[0] == null);
-    if(template_is_only_text) {
-        throw new Error(EntryAdditionView.INVALID_TEMPLATE);
-    }
-    
-    EntryAdditionView.template_html = file_content;
-};
-
-EntryAdditionView.onTemplateFileError = function(file_content) {
-    throw new Error(EntryAdditionView.TEMPLATE_LOAD_ERROR);
 };
 
 EntryAdditionView.prototype.initializeForm = function() {
--- trunk/js/views/ListCreationView.js
+++ trunk/js/views/ListCreationView.js
@@ -2,55 +2,12 @@ var ListCreationView = function() {
     this.initialize();
 };
 
-ListCreationView.TEMPLATE_LOAD_ERROR = "Could not load tempate file.";
-ListCreationView.INVALID_TEMPLATE = "Invalid template.";
-
 ListCreationView.template_file = "ListCreationView.html";
-ListCreationView.template_html = null;
 
 ListCreationView.prototype.initialize = function() {
-    this.element = ListCreationView.getElementFromTemplateHtml();
+    this.element = ViewLoader.getElementFromTemplateFile(ListCreationView.template_file);
     this.initializeForm();
     this.initializeSaveButton();
-};
-
-ListCreationView.getElementFromTemplateHtml = function() {
-    var static_template_not_loaded = (ListCreationView.template_html == null);
-    if(static_template_not_loaded) {
-        ListCreationView.loadTemplateHtml();
-    }
-    
-    var auxiliar = document.createElement("div");
-    auxiliar.innerHTML = ListCreationView.template_html;
-    
-    var element = auxiliar.firstChild;
-    
-    return element;
-};
-
-ListCreationView.loadTemplateHtml = function() {
-    $.ajax({
-        url: Config.HtmlTemplatesPath + "/" + ListCreationView.template_file,
-        async: false,
-        success: ListCreationView.onTemplateFileLoaded,
-        error: ListCreationView.onTemplateFileError
-    });
-};
-
-ListCreationView.onTemplateFileLoaded = function(file_content) {
-    var auxiliar = document.createElement("div");
-    auxiliar.innerHTML = file_content;
-    
-    var template_is_only_text = (auxiliar.children[0] == null);
-    if(template_is_only_text) {
-        throw new Error(ListCreationView.INVALID_TEMPLATE);
-    }
-    
-    ListCreationView.template_html = file_content;
-};
-
-ListCreationView.onTemplateFileError = function(file_content) {
-    throw new Error(ListCreationView.TEMPLATE_LOAD_ERROR);
 };
 
 ListCreationView.prototype.initializeForm = function() {
