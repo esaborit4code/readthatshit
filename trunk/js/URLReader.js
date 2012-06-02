c489261e15be9e91ff02cf15058d9c4b0be56143
saborit.eduardo@gmail.com <saborit.eduardo@gmail.com@b9ac0532-b296-deb0-b7b3-8a33a806bde0>
2012-06-02 19:00:13 +0000

    Added functionality to URLReader

--- trunk/js/URLReader.js
+++ trunk/js/URLReader.js
@@ -1,20 +1,26 @@
 function URLReader() { };
 
 URLReader.getParameters = function(url, section_separator, parameter_separator) {
+    var hash = URLReader.getHash(url, section_separator);
+    
+    var there_are_parameters = (hash != "");
+    if(!there_are_parameters) {
+        return [];
+    }
+    
+    var parameters = hash.split(parameter_separator);
+    
+    return parameters;
+};
+
+URLReader.getHash = function (url, section_separator) {
     var url_has_section_separator = (url.indexOf(section_separator) > -1);
     if(!url_has_section_separator) {
-        return [];
+        return "";
     }
     
     var url_split = url.split(section_separator);
     var parameter_section = url_split[1];
     
-    var there_are_parameters = (parameter_section != "");
-    if(!there_are_parameters) {
-        return [];
-    }
-    
-    var parameters = parameter_section.split(parameter_separator);
-    
-    return parameters;
+    return parameter_section;
 };
--- trunk/test/js/spec/URLReaderSpec.js
+++ trunk/test/js/spec/URLReaderSpec.js
@@ -1,8 +1,9 @@
 describe("URLReader", function() {
   var section_separator = "#";
   var parameter_separator = "/";
-   
-  it("should return the parameters after the section separator using a determined char to sepparate parameters", function() {
+
+  
+  it("should return the parameters after the section separator using a determined char to sepparate parameters as an array", function() {
     var first_parameter = "first";
     var second_parameter = "second";
     var the_url = "example#" + first_parameter + "/" + second_parameter;
@@ -10,6 +11,14 @@ describe("URLReader", function() {
     var the_parameters = URLReader.getParameters(the_url, section_separator, parameter_separator);
     expect(the_parameters[0]).toEqual(first_parameter);
     expect(the_parameters[1]).toEqual(second_parameter);
+  });
+  
+  it("should return the parameters after the section separator using a determined char to sepparate parameters as a string", function() {
+    var parameters = "first/second";
+    var the_url = "example#" + parameters;
+    
+    var hash = URLReader.getHash(the_url, section_separator);
+    expect(hash).toEqual(parameters);
   });
   
   it("should return an empty array if url has no parameter separator", function () {
