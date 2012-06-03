fe917d1c26a5ab828fc3d8fb23e0f435967c3f66
saborit.eduardo@gmail.com <saborit.eduardo@gmail.com@b9ac0532-b296-deb0-b7b3-8a33a806bde0>
2012-06-03 12:07:36 +0000

    Improved Router. Now it can route to hashes that start with a registered hash

--- trunk/js/Router.js
+++ trunk/js/Router.js
@@ -6,9 +6,14 @@ Router.HASH_NOT_REGISTERED = "Hash is not registered";
 Router.routes = {};
 
 Router.register = function(hash, callback, force_overwrite) {
-    var hash_is_already_registered = (Router.routes[hash] != null);
-    if(hash_is_already_registered && !force_overwrite) {
+    var matching_hash = Router.findMatchingHashForHash(hash);
+    var there_is_a_matching_hash = (matching_hash != null);
+    if(there_is_a_matching_hash && !force_overwrite) {
         throw new Error(Router.HASH_ALREADY_REGISTERED);
+    }
+    
+    if(there_is_a_matching_hash && force_overwrite) {
+        Router.routes[matching_hash] = null;
     }
     
     Router.routes[hash] = callback;
@@ -18,10 +23,27 @@ Router.route = function(url) {
     var hash = URLReader.getHash(url);
     var parametrized_hash = URLReader.getParameters(url);
     
-    var hash_is_already_registered = (Router.routes[hash] != null);
-    if(!hash_is_already_registered) {
+    var matching_hash = Router.findMatchingHashForHash(hash);
+    
+    var hash_is_registered = (matching_hash != null);
+    if(!hash_is_registered) {
         throw new Error(Router.HASH_NOT_REGISTERED);
     }
 
-    Router.routes[hash](parametrized_hash);
+    Router.routes[matching_hash](parametrized_hash);
+};
+
+Router.findMatchingHashForHash = function(hash) {
+    var matching_hash = null;
+    
+    for(var registered_hash in Router.routes) {
+        var hash_matches = (hash.indexOf(registered_hash) == 0);
+        var hash_has_callback = (Router.routes[registered_hash] != null)
+        if(hash_matches && hash_has_callback) {
+            matching_hash = registered_hash;
+            break;
+        }
+    }
+      
+    return matching_hash;
 };
